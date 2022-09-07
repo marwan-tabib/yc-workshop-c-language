96f250531cca5ceca8a9e7d053f5d295161ec57b
marwan-tabib <tabibmarouwane6@gmail.com>
2022-09-07 17:05:04 +0200

    Challenge 2 : Conversion du °C au °F

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main() {
+    
+   // Challenge 2 : Conversion du °C au °F
+	
+	// Varaibles
+		float F ; 
+		
+	// Show
+		printf("Add C° value :");
+		scanf("%f" , &F);
+		
+		printf("C° Result %f" , (F - 32) / 1.8 );
+		
+   return 0;
+}
