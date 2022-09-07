38584ecee7dc8e4e9c80aa014cb2093258164033
marwan-tabib <tabibmarouwane6@gmail.com>
2022-09-07 17:29:20 +0200

    Challenge 4 : Conversion du Mile au Km

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 void main() {
     
-   // Challenge 3 : Conversion du Mile au Km
+   // Challenge 4 : Conversion du Mile au Km
+   // Ecrivez un programme qui demande la distance en Mile et la transforme en Metre La formule :
 	
 	// Varaibles
-		float Metre ; 
+		float Mile; 
 		
 	// Show
 		printf("Add C° value :");
 		scanf("%f" , &Metre);
 		
-		printf("C° Result %f" , Metre * 0.000621371 );
+		printf("C° Result %f" , Mile * 1609.34 );
 		
    return 0;
 }
