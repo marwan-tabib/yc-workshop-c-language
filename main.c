699ebd49668d92cfa6ee01fc5f1bfa1b095d07df
marwan-tabib <tabibmarouwane6@gmail.com>
2022-09-09 10:22:55 +0200

    Challenge 8 : Moyenne et somme de 4 nombres

--- main.c
+++ main.c
@@ -1,39 +1,17 @@
 #include <stdio.h>
 void main() {
     
-   // Challenge 7 : Calcule et affichage résultat en format décimal
+   // Challenge 8 : Moyenne et somme de 4 nombres
    // a et b sont deux entiers saisies au clavier, calculez et affichez a+b, a-b, a*b, a/b, a%b en format décimal, et en soignant l’interface homme/machine. (a/b donne le quotient de la division, a%b donne le reste de la division)
 	
 	// Varaibles
-	    char operation;
-	    double n1, n2;
+	    double n1, n2 , n3 , n4 , somme;
 		
 	// Show	
-	    printf("Enter Calculate (Just : +, -, *, /): ");
-	    scanf("%lf %c %lf",&n1, &operation ,  &n2);
-	
-	    switch(operation)
-	    {
-	        case '+':
-	            printf("%.2lf + %.2lf = %.2lf",n1, n2, n1+n2);
-	            break;
-	
-	        case '-':
-	            printf("%.2lf - %.2lf = %.2lf",n1, n2, n1-n2);
-	            break;
-	
-	        case '*':
-	            printf("%.2lf * %.2lf = %.2lf",n1, n2, n1*n2);
-	            break;
-	
-	        case '/':
-	            printf("%.2lf / %.2lf = %.2lf",n1, n2, n1/n2);
-	            break;
-	
-	        // operator doesn't match any case constant +, -, *, /
-	        default:
-	            printf("Error! operator is not correct");
-	    }
+	    printf("Enter numbers (Ex : 13 23 45 53): ");
+	    scanf("%lf %lf %lf %lf",&n1 , &n2 , &n3 , &n4);
+	    somme = n1 + n2 + n3 + n4 ;
+		printf("Result : %2.lf" , somme / 4);
 		
    return 0;
 }
