8eae9fa570a74dbe9e63dbd14cc20e50e2d5c39e
marwan-tabib <tabibmarouwane6@gmail.com>
2022-09-09 10:16:37 +0200

    Challenge 2 : Conversion du �C au �F

--- main.c
+++ main.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 void main() {
-   // Challenge 1 : Affichage Informations
-   
-	// Varaibles
-    char first_name[20];
-    char last_name[20];
-    int phone;
-    char gender[20];
     
-    // Show
-	    printf("Enter Your First Name: ");
-	    scanf("%s", &first_name);
-	    getchar();
-	    printf("Enter Your Last Name: ");
-	    scanf("%s", &last_name);
-	    getchar();
-	    printf("Enter Your Phone: ");
-	    scanf("%d", &phone);  
-	    getchar();
-	    printf("Enter Your Gender: ");
-	    scanf("%s", &gender);  
-	    
-	    printf("I'm %s %s (%s) , My phone number is %d " , first_name , last_name , gender , phone);
+   // Challenge 2 : Conversion du �C au �F
+	
+	// Varaibles
+		float F ; 
+		
+	// Show
+		printf("Add F� value :");
+		scanf("%f" , &F);
+		
+		printf("C� Result %f" , (F - 32) / 1.8 );
+		
    return 0;
 }
