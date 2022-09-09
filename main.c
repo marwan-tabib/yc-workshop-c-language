af8989b1df418cbd9dfa4073b2da04f8523ef5bd
marwan-tabib <tabibmarouwane6@gmail.com>
2022-09-09 10:24:58 +0200

    Challenge 10 : Circonférence d'un cercle

--- main.c
+++ main.c
@@ -2,20 +2,17 @@
 #include <math.h>
 void main() {
     
-   // Challenge 9 : Distance entre deux points
-   // Ecrivez un programme C pour trouver la distance entre deux points donnés. La formule de distance est dérivée du théorème de Pythagore. Pour trouver la distance entre deux points (x1, y1) et (x2, y2), il suffit d'utiliser les coordonnées de ces paires ordonnées et d'appliquer la formule. Formule de distance Supposons que nous ayons deux points M et N, dont les coordonnées sont respectivement (x1, y1) et (x2, y2). Leur distance peut être représentée par MN et peut être calculée selon la formule ci-dessous, Le premier point (M):- (x1, y1) Deuxième point (N) : (x2, y2) Distance (MN):- v((x2-x1)² + (y2-y1)²) Exemple:- M = (4, 8) N = (12, 14) Alors la distance entre M et N est MN = v((12-4)² + (14-8)²) = v(64 + 36) = v(100) = 10
-   
+   // Challenge 10 : Circonférence d'un cercle
+	// Écrivez un programme pour trouver la circonférence d'un cercle. Prenez le rayon du cercle en entrée de l'utilisateur. La formule pour, la circonférence du cercle = 2pr ; où r est le rayon.
+	   
 	// Varaibles
-	    double x1, x2 , y1 , y2 , M , N , MN;
+	    double r;
 		
 	// Show			
-	    printf("Enter numbers x1 and x2 (Ex : 7 3 ): ");
-	    scanf("%lf %lf",&x1 , &x2 );
-	    printf("Enter numbers y1 and y2 (Ex : 8 4 ): ");
-	    scanf("%lf %lf",&y1 , &y2 );
-	    M = pow(x1 - y1 , 2);
-	    N = pow(x2 - y2 , 2);
-	    MN = sqrt(M + N);
-	    printf("Result : %.2lf " , MN);
+	    printf("Enter rayon value: ");
+	    scanf("%lf", &r );
+	    
+	    printf("Circonférence cercle  : %.2lf " , 2*3.14*r);
+	    
    return 0;
 }
