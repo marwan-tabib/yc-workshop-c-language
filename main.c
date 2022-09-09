c16ace05c6e0ec0854d4bbd69858f79fece48e2c
marwan-tabib <tabibmarouwane6@gmail.com>
2022-09-09 10:19:47 +0200

    Challenge 6 : Conversion du Fahrenheit au Celsius

--- main.c
+++ main.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
 void main() {
     
-   // Challenge 5 : Affichage Température
-   // Ecrivez un programme qui demande la température en Fahrenheit et la transforme en degré Celsius et affiche la sensation ressentie (tr�s froid, froid, chaud, très chaud) La formule :
+   // Challenge 6 : Conversion du Fahrenheit au Celsius
+   // Ecrivez un programme qui demande la température en Fahrenheit et la transforme en degré Celsius La formule :
 	
 	// Varaibles
-		float F ; 
+		float F; 
 		
 	// Show
-		printf("Add F� value :");
-		scanf("%f" , &F );
+		printf("Add Fahrenheit value :");
+		scanf("%f" , &F);
 		
-		if (F < 18)
-			{
-			  printf ("Tr�s  froid !");
-			}
-		if (F >= 18)
-			{
-				if (F >= 35){
-						if (F >= 45){ printf ("Tr�s chaud !"); }
-					 	else { printf ("Chaud"); }
-					}
-				else { printf ("Froid"); }
-			}
+		printf("Celsius Result %f" , (F-32)/1.8 );
 		
    return 0;
 }
