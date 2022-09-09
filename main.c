b8c9b3585dcfc3f9a413d2bd96cc4240129d38df
marwan-tabib <tabibmarouwane6@gmail.com>
2022-09-09 10:20:39 +0200

    Challenge 7 : Calcule et affichage résultat en format décimal

--- main.c
+++ main.c
@@ -1,17 +1,39 @@
 #include <stdio.h>
 void main() {
     
-   // Challenge 6 : Conversion du Fahrenheit au Celsius
-   // Ecrivez un programme qui demande la température en Fahrenheit et la transforme en degré Celsius La formule :
+   // Challenge 7 : Calcule et affichage résultat en format décimal
+   // a et b sont deux entiers saisies au clavier, calculez et affichez a+b, a-b, a*b, a/b, a%b en format décimal, et en soignant l’interface homme/machine. (a/b donne le quotient de la division, a%b donne le reste de la division)
 	
 	// Varaibles
-		float F; 
+	    char operation;
+	    double n1, n2;
 		
-	// Show
-		printf("Add Fahrenheit value :");
-		scanf("%f" , &F);
-		
-		printf("Celsius Result %f" , (F-32)/1.8 );
+	// Show	
+	    printf("Enter Calculate (Just : +, -, *, /): ");
+	    scanf("%lf %c %lf",&n1, &operation ,  &n2);
+	
+	    switch(operation)
+	    {
+	        case '+':
+	            printf("%.2lf + %.2lf = %.2lf",n1, n2, n1+n2);
+	            break;
+	
+	        case '-':
+	            printf("%.2lf - %.2lf = %.2lf",n1, n2, n1-n2);
+	            break;
+	
+	        case '*':
+	            printf("%.2lf * %.2lf = %.2lf",n1, n2, n1*n2);
+	            break;
+	
+	        case '/':
+	            printf("%.2lf / %.2lf = %.2lf",n1, n2, n1/n2);
+	            break;
+	
+	        // operator doesn't match any case constant +, -, *, /
+	        default:
+	            printf("Error! operator is not correct");
+	    }
 		
    return 0;
 }
