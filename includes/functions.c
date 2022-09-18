e17cfd02355e22f67d04e77e0edd4ca132db0ef2
marwan-tabib <marwanmaroc297@gmail.com>
2022-09-18 19:52:05 +0200

    finish products sections

--- includes/functions.c
+++ includes/functions.c
@@ -25,25 +25,31 @@
 						chose == 1 ? intro() : chose == 2 ? produitZone() : printf("please tree again");
 						break;
 					}
-					case 2 : {	
+					case 2 : {
 		    			printf("\n\n-- Chose --");
 			    		printf("\n1 - Go to genrale menu ->");
 			    		printf("\n entrer un numbre : ");
 			    		scanf("%d" , &chose);
-						chose == 1 ? system("exit") : printf("please tree again");
+						chose == 1 ? intro() : printf("please tree again");
 						break;
 					}
-//					case 2 : {	
-//		    			printf("\n\n-- Chose --");
-//			    		printf("\n1 - Go Exit ->");
-//			    		printf("\n entrer un numbre : ");
-//			    		scanf("%d" , &chose);
-//						chose == 1 ? system("exit") : printf("please tree again");
-//						break;
-//					}
+					default : system("exit");
 				}
 			}
+		// Get Date
+			date getDate(){
+				date d;
+				time_t t = time(NULL);
+				struct tm tm = *localtime(&t);
+				d.day = tm.tm_mday;
+				d.month = tm.tm_mon + 1;
+				d.year = tm.tm_year + 1900;
+				return d;
+			  
+			}
 	//////---- End Plugins ----/////
+	
+	
 	
 	//////---- Functions ----//////
 		// Add product
@@ -59,12 +65,16 @@
 					}else{
 						printf("* how match you added products ? : ");
 						scanf("%d" , &n);
+						
 						system("cls");
+						
 						printf("***		Section de les produits -> Ajouter plusieurs produits		***");
 						printf("\n You Can Add : %d Products\n" , n);
 					}
 						
 					for(i=0; i<n ; i++){
+						
+	    				T[i].ttc = T[i].prix + T[i].prix * 0.15;
 						printf("\n Nome de produit %d : " , T[z].code);
 						scanf("%s" , T[z].nome);
 						
@@ -74,65 +84,153 @@
 						printf("\n Prix de produit %d : " , T[z].code);
 						scanf("%f" , &T[z].prix);
 						
-						printf("\nId: %d | Nome: %s | Quantite: %d | Prix: %.2f DH \n" , T[z].code , T[z].nome , T[z].quantite , T[z].prix);
+						printf("\nId: %d | Nome: %s | Quantite: %d | Prix: %.2f DH \n" , T[z].code , T[z].nome , T[z].quantite , T[z].prix );
 						(*size)++;
 						z = *(size);
 						T[z].code = z + 1;
 					}
-					
 					delay(1);
-					/* 1 : produit zone	*/ 
 					goBack(1);
 			}
     	// List product
-    		void listTousProduits(produit *T, int *size){
+    		void listTousProduits(bool back , produit *T, int *size){
 				system("cls");
+				
 				// Variables
-	    			int i , z = *(size);
-	    			
-						printf("***		Section de les produits -> List tous produits		***");
-						printf("\n Total Products : %d .\n" , z);
+	    			int i , z = *(size) ;
+	    		
+	    		// Show
+					printf("***		Section de les produits -> List tous produits		***");
+					printf("\n Total Products : %d .\n" , z);
 	    			
 					for(i=0; i<z ; i++){
-						printf("\nId: %d | Nome: %s | Quantite: %d | Prix: %.2f DH \n" , T[i].code , T[i].nome , T[i].quantite , T[i].prix);
+	    				T[i].ttc = T[i].prix + T[i].prix * 0.15;
+						printf("\nId: %d | Nome: %s | Quantite: %d | Prix: %.2f DH | TTC : %f \n" , T[i].code , T[i].nome , T[i].quantite , T[i].prix , T[i].ttc);
 					}
-					delay(1);
-					/* 1 : produit zone	*/ 
-					goBack(1);
+					if(back == 1){
+						delay(1);
+						goBack(1);
+					}
+					
 			}
     	// Buy product
-    		void acheterProduit(){printf("4");}
+    		void acheterProduit(produit *T , int *size){
+				system("cls");
+				// Variables
+					int i , z = *(size) , n , check;
+				
+				// Show 
+					if(z==0){
+						printf("No Product , first add product !");
+						goBack(1);
+					}else{
+						listTousProduits(0 , T , size);
+						printf("Enter product code to buy : ");
+						scanf("%d" , &n);
+						n--;T[n].quantite--;
+						T[n].d = getDate();
+					}
+					delay(1);
+					goBack(1);
+			}
+		// Recherche Produit
+			void rechercheProduit(produit *T , int *size){
+				system("cls");
+				// Variables
+					int i , z = *(size) , n , check;
+				
+				// Show 
+					if(z==0){
+						printf("No Product , first add product !");
+						goBack(1);
+					}else{
+						printf(" 1  - Search with code : \n");
+						printf(" 2  - Search with quantity : \n ");
+						
+			        	printf("\n entrer un numbre [0 - Go back]: ");
+						scanf("%d" , &chose);
+						
+						switch(chose){
+							case 0 : produitZone();
+							case 1 : {
+								system("cls");
+								printf("Enter product code to search : ");
+								scanf("%d" , &n);
+								int m = n;
+								n--;
+								if(m == T[n].code){
+		  							printf("\nId: %d | Nome: %s | Quantite: %d | Prix: %.2f DH | TTC : %f \n" , T[n].code , T[n].nome , T[n].quantite , T[n].prix , T[n].ttc);
+									delay(1);
+									goBack(1);
+								}
+								else {
+									printf("No Result , Please three again...");
+									goBack(1);
+								}
+								break;
+							}
+							case 2 : {
+								system("cls");
+								printf("Enter product quantity value: ");
+								scanf("%d" , &n);
+								printf("%d" , z);
+								for(i=0 ; i < z ; i++){
+									if(n == T[i].quantite){
+										printf("\nId: %d | Nome: %s | Quantite: %d | Prix: %.2f DH | TTC : %f \n" , T[i].code , T[i].nome , T[i].quantite , T[i].prix , T[i].ttc);
+									}else {
+										printf("No Result , Please three again...");
+										goBack(1);
+									}
+								}
+										delay(1);
+										goBack(1);
+								break;
+							}
+							default: rechercheProduit(res , &s);
+						}
+					}
+					delay(1);
+					goBack(1);
+			}
 	//////---- End Functions ----//////
 
 	//////---- Sections ----//////
     	produitZone(){
-    			system("cls");
+    		system("cls");
 
     		// Variables
 	    		produit produit;
 	    		
 	    	// Shows
 		    	printf("***		Ajouter un nouveau produit		***");
-				printf("\n1  - Ajouter un nouveau produit:\n");
-				printf("2  - Ajouter plusieurs nouveaux produits:\n");
-				printf("3  - Lister tous les produits:\n");
+				printf("\n[ 1 ]  - Ajouter un nouveau produit:\n");
+				printf("[ 2 ]  - Ajouter plusieurs nouveaux produits:\n");
+				printf("[ 3 ]  - Lister tous les produits:\n");
+				printf("[ 4 ]  - Acheter produit:\n");
+				printf("[ 5 ]  - Rechercher les produits Par:\n");
 				
-	        	printf("\n entrer un numbre [0 to exit]: ");
+	        	printf("\n entrer un numbre [0 - Go back]: ");
 				scanf("%d" , &chose);
 				
-			
 				switch(chose){
-					case 0: system("exit");
+					case 0: intro();
 						break;
 					case 1: ajouterProduit(0 , res , &s);
 						break;
 					case 2: ajouterProduit(1 , res , &s);
 						break;
-					case 3: listTousProduits(res , &s);
+					case 3: listTousProduits(1 , res , &s);
 						break;
+					case 4: acheterProduit(res , &s);
+						break;
+					case 5: rechercheProduit(res , &s);
+						break;
+					default: produitZone();
 				}
 		}
 	//////---- End Sections ----//////
+	
+	
 	//////---- Main ----//////	
 		intro(){
 			system("cls");
@@ -140,27 +238,21 @@
 			
 			// shows
 				printf("|*/*/*/*/*/*| Gestion de Pharmacie |*/*/*/*/*/*|\n\n");
-				printf("1  - Section de les produits:\n");
-				printf("2  - Acheter produit:\n");
-				printf("3  - Rechercher les produits Par:\n");
-				printf("4  - Etat du stock:\n");
-				printf("5  - Alimenter le stock:\n");
-				printf("6  - Supprimer les produits par:\n");
-				printf("7  - Statistique de vente:\n");
+				printf("[- 1 -]  - Section de les produits:\n");
+				printf("[- 4 -]  - Etat du stock:\n");
+				printf("[- 5 -]  - Alimenter le stock:\n");
+				printf("[- 6 -]  - Supprimer les produits par:\n");
+				printf("[- 7 -]  - Statistique de vente:\n");
+				
 		        printf("\n entrer un numbre [0 to exit] : ");
 				scanf("%d",&chose);
 			
-			switch(chose){
-				case 0 : system("exit");
-						break;
-				case 1 : produitZone();
-						break;
-	//			case 2 : ajouterPlusieursProduits();
-	//					break;
-	//			case 3 : listTousProduits();
-	//					break;
-	//			case 4 : acheterProduit();
-	//					break;
-			} 
+				switch(chose){
+					case 0 : system("exit");
+							break;
+					case 1 : produitZone();
+							break;
+					default : intro();
+				} 
 		}
 	//////---- End Main ----//////
--- includes/structs.c
+++ includes/structs.c
@@ -1,6 +1,11 @@
 
+	typedef struct Date{
+		int day , month , year;
+	} date ;
+	
 	typedef struct produit{
 		int code , quantite ;
-		char nome[20];
-		float prix;
+		char nome[20]  ;
+		date d;
+		float prix , ttc;
 	} produit ;
